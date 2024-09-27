12871f4c51c54d64323872dbda3f187c046bc131
Shashidharak89 <shashidharak334@gmail.com>
2024-09-27 20:02:56 +0530

    Remove bugs

--- script.js
+++ script.js
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
+
 var loader=document.getElementById("preloader")
 var login=document.getElementById("loginpage")
 window.addEventListener("load",function(){
     loader.style.display="none"
     login.style.display="block"
-=======
-var loader=document.getElementById("preloader")
-var login=document.getElementById("loginpage")
-window.addEventListener("load",function(){
-    loader.style.display="none"
-    login.style.display="block"
->>>>>>> 9d35ac8ac0aa2d3d3ed91c3cf0f19a8a3fabb4f1
 })
