b4b166a9fbf52fe150300fe35f68ae23e00e804d
Shashidharak89 <shashidharak334@gmail.com>
2024-09-27 19:47:59 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+var loader=document.getElementById("preloader")
+var login=document.getElementById("loginpage")
+window.addEventListener("load",function(){
+    loader.style.display="none"
+    login.style.display="block"
+})
